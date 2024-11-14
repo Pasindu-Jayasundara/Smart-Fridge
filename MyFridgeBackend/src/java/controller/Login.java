bf3b72b983085000c6eb3e1a048aba5cd4c06ba9
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2024-11-14 21:59:06 +0530

    feat: implement login filter for validation, enhance login servlet with fridge and power consumption data retrieval

--- MyFridgeBackend/src/java/controller/Login.java
+++ MyFridgeBackend/src/java/controller/Login.java
@@ -1,87 +1,127 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import dto.Response_DTO;
+import entity.Fridge;
+import entity.Power_consumption;
+import entity.Rack_weight;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.HibernateUtil;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
-/**
- *
- * @author Asus
- */
 @WebServlet(name = "Login", urlPatterns = {"/Login"})
 public class Login extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Login</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Login at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String fridgeCode = (String) request.getAttribute("fridgeCode");
+        String password = (String) request.getAttribute("password");
+
+        String message = "";
+        boolean isSuccess = false;
+
+        Session hibernateSession = HibernateUtil.getSessionFactory().openSession();
+        JsonObject replyObj = new JsonObject();
+        Gson gson = new Gson();
+
+        // fridge
+        Criteria fridgeCriteria = hibernateSession.createCriteria(Fridge.class);
+        fridgeCriteria.add(Restrictions.and(
+                Restrictions.eq("code", fridgeCode),
+                Restrictions.eq("password", password))
+        );
+        Fridge fridge = (Fridge) fridgeCriteria.uniqueResult();
+
+        if (fridge != null) {
+            replyObj.add("fridge", gson.toJsonTree(fridge));
+            message = "Login Success";
+            isSuccess = true;
+        } else {
+            message = "Invalid Details";
         }
-    }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        if (fridge != null) {
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+            // rack weight
+            Criteria rackWeightCriteria = hibernateSession.createCriteria(Rack_weight.class);
+            rackWeightCriteria.add(Restrictions.eq("fridge", fridge));
+            List<Rack_weight> rackWeightList = rackWeightCriteria.list();
+            if (!rackWeightList.isEmpty()) {
+                
+                JsonObject rackWeightObj = new JsonObject();
+                for (Rack_weight rack_weight : rackWeightList) {
+                    if(rack_weight.getRack_number().getRack_number()==1){
+                        
+                        JsonObject rackOne = new JsonObject();
+                        rackOne.addProperty("weight", rack_weight.getWeight());
+                        
+                        rackWeightObj.add("rackOne", gson.toJsonTree(rackOne));
+                        
+                    }else{
+                        JsonObject rackTwo = new JsonObject();
+                        rackTwo.addProperty("weight", rack_weight.getWeight());
+                        
+                        rackWeightObj.add("rackTwo", gson.toJsonTree(rackTwo));
+                    }
+                }
+                replyObj.add("rackWeight", rackWeightObj);
+            }
+            
+            // power consumption
+            Criteria powerConsumptionCriteria = hibernateSession.createCriteria(Power_consumption.class);
+            powerConsumptionCriteria.add(Restrictions.eq("fridge", fridge));
+            List<Power_consumption> powerConsumptionList = powerConsumptionCriteria.list();
+            if (!powerConsumptionList.isEmpty()) {
+                
+                JsonObject powerConsumptionObj = new JsonObject();
+                for (Power_consumption power_consumption : powerConsumptionList) {
+                    if(rack_weight.getRack_number().getRack_number()==1){
+                        
+                        JsonObject rackOne = new JsonObject();
+                        rackOne.addProperty("weight", rack_weight.getWeight());
+                        
+                        rackWeightObj.add("rackOne", gson.toJsonTree(rackOne));
+                        
+                    }else{
+                        JsonObject rackTwo = new JsonObject();
+                        rackTwo.addProperty("weight", rack_weight.getWeight());
+                        
+                        rackWeightObj.add("rackTwo", gson.toJsonTree(rackTwo));
+                    }
+                }
+                replyObj.add("rackWeight", rackWeightObj);
+            }
+            
+            
+            
+        }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+        if (isSuccess) {
+            jo.add("fridge", gson.toJsonTree(fridge));
+
+            jo.addProperty("profileImage", user.getProfile_image());
+            jo.addProperty("profileAbout", user.getAbout());
+        } else {
+            jo.addProperty("msg", message);
+        }
+
+        hibernateSession.close();
+
+        Response_DTO response_DTO = new Response_DTO(isSuccess, gson.toJsonTree(jo));
+
+        response.setContentType("application/json");
+        response.getWriter().write(gson.toJson(response_DTO));
+
+    }
 
 }
--- MyFridgeBackend/src/java/model/filter/LoginFilter.java
+++ MyFridgeBackend/src/java/model/filter/LoginFilter.java
@@ -1,13 +1,93 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model.filter;
 
-/**
- *
- * @author Asus
- */
-public class LoginFilter {
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import dto.Response_DTO;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import model.Validation;
+
+
+@WebFilter(urlPatterns = {"/Login"})
+public class LoginFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        Gson gson = new Gson();
+        JsonObject fromJson = gson.fromJson(request.getReader(), JsonObject.class);
+
+        boolean isInvalid = false;
+        String errorMessage = "";
+
+        if (!fromJson.has("fridgeCode")) {
+
+            isInvalid = true;
+            errorMessage = "Fridge Code Cannot Be Found";
+
+        } else if (!fromJson.has("password")) {
+
+            isInvalid = true;
+            errorMessage = "Password Cannot Be Found";
+
+        } else {
+
+            String fridgeCode = fromJson.get("fridgeCode").getAsString();
+            String password = fromJson.get("password").getAsString();
+
+            if (fridgeCode == null || fridgeCode.trim().equals("")) {
+                //no code
+                isInvalid = true;
+                errorMessage = "Missing Fridge Code";
+
+            } else if (password == null || password.trim().equals("")) {
+                //no password
+                isInvalid = true;
+                errorMessage = "Missing Password";
+
+            } else {
+
+                if (password.length() > 20) {
+                    //password too long
+                    isInvalid = true;
+                    errorMessage = "Password Too Long";
+
+                } else if (!Validation.isValidPassword(password)) {
+                    //invalid password
+                    isInvalid = true;
+                    errorMessage = "Invalid Password Format";
+
+                } else {
+
+                    request.setAttribute("fridgeCode", fridgeCode);
+                    request.setAttribute("password", password);
+
+                    chain.doFilter(request, response);
+                }
+
+            }
+        }
+
+        if (isInvalid) {
+            Response_DTO response_DTO = new Response_DTO(false, errorMessage);
+            response.setContentType("application/json");
+            response.getWriter().write(gson.toJson(response_DTO));
+        }
+    }
+
+
+    @Override
+    public void destroy() {
+    }
     
 }
